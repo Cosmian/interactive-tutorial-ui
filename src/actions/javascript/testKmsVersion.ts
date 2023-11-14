f584f4cd5fd1e441b435509598e4629dcea2a4f6
Laetitia Langlois <laetitia.langlois@cosmian.com>
2023-11-14 15:32:25 +0100

    add auth on kms version check

--- src/actions/javascript/testKmsVersion.ts
+++ src/actions/javascript/testKmsVersion.ts
@@ -5,8 +5,7 @@ import { BACKEND_URL } from "./backendConfig";
 // Get KMS version
 //
 export const getKmsVersion = async (kmsToken: string): Promise<string> => {
-  console.log(kmsToken);
-  const client = new KmsClient(BACKEND_URL);
+  const client = new KmsClient(BACKEND_URL, kmsToken);
   const version = await client.version();
   return version.text();
 };
