2089701d507344681bfe445a29f488edd396a87d
Laetitia Langlois <laetitia.langlois@cosmian.com>
2023-11-14 10:41:44 +0100

    test kms version without authentication

--- src/actions/javascript/testKmsVersion.ts
+++ src/actions/javascript/testKmsVersion.ts
@@ -5,7 +5,8 @@ import { BACKEND_URL } from "./backendConfig";
 // Get KMS version
 //
 export const getKmsVersion = async (kmsToken: string): Promise<string> => {
-  const client = new KmsClient(BACKEND_URL, kmsToken);
+  console.log(kmsToken);
+  const client = new KmsClient(BACKEND_URL);
   const version = await client.version();
   return version.text();
 };
