69ba3ecd40206ab0ee18989288049b323e8d8618
ccorsin <celia.corsin@gmail.com>
2024-11-13 14:24:24 +0100

    Edit menu order

--- src/utils/routePathsConfig.tsx
+++ src/utils/routePathsConfig.tsx
@@ -1,35 +1,35 @@
-import OverView from "../pages/OverView";
-import AuditSnapshot from "../pages/confidentialVm/AuditSnapshot";
-import DeployApplication from "../pages/confidentialVm/DeployApplication";
-import DetectMalware from "../pages/confidentialVm/DetectMalware";
-import SnapshotVm from "../pages/confidentialVm/SnapshotVm";
-import UseConfidentialVm from "../pages/confidentialVm/UseConfidentialVm";
-import VerifyIntegrity from "../pages/confidentialVm/VerifyIntegrity";
-import AboutCovercrypt from "../pages/covercrypt/AboutCovercrypt";
-import CreateEncryptionPolicy from "../pages/covercrypt/CreateEncryptionPolicy";
-import CreateMasterKeyPair from "../pages/covercrypt/CreateMasterKeyPair";
-import DecryptData from "../pages/covercrypt/DecryptData";
-import EncryptData from "../pages/covercrypt/EncryptData";
-import GenerateUserDecryptionKey from "../pages/covercrypt/GenerateUserDecryptionKey";
-import RefreshKeys from "../pages/covercrypt/RefreshKeys";
-import SetupCovercrypt from "../pages/covercrypt/SetupCovercrypt";
-import AboutCse from "../pages/cse/AboutCse";
-import SetupCse from "../pages/cse/SetupCse";
-import SummarizeDocument from "../pages/cse/SummarizeDocument";
-import AboutFindex from "../pages/findex/AboutFindex";
-import IndexDatabase from "../pages/findex/IndexDatabase";
-import InstantiateFindex from "../pages/findex/InstantiateFindex";
-import SearchInDatabase from "../pages/findex/SearchInDatabase";
-import SetupFindex from "../pages/findex/SetupFindex";
-import AboutPKI from "../pages/pki/AboutPKI";
-import DecryptDataPKI from "../pages/pki/DecryptDataPKI";
-import EncryptDataPki from "../pages/pki/EncryptDataPki";
-import GetCertificate from "../pages/pki/GetCertificate";
-import GrantAccess from "../pages/pki/GrantAccess";
-import ImportAndUnwrapUDK from "../pages/pki/ImportAndUnwrapUDK";
-import SendWrappedDecryptionKey from "../pages/pki/SendWrappedDecryptionKey";
-import SetupPki from "../pages/pki/SetupPki";
-import UploadCert from "../pages/pki/UploadCert";
+import OverView from "../pages/OverView"
+import AuditSnapshot from "../pages/confidentialVm/AuditSnapshot"
+import DeployApplication from "../pages/confidentialVm/DeployApplication"
+import DetectMalware from "../pages/confidentialVm/DetectMalware"
+import SnapshotVm from "../pages/confidentialVm/SnapshotVm"
+import UseConfidentialVm from "../pages/confidentialVm/UseConfidentialVm"
+import VerifyIntegrity from "../pages/confidentialVm/VerifyIntegrity"
+import AboutCovercrypt from "../pages/covercrypt/AboutCovercrypt"
+import CreateEncryptionPolicy from "../pages/covercrypt/CreateEncryptionPolicy"
+import CreateMasterKeyPair from "../pages/covercrypt/CreateMasterKeyPair"
+import DecryptData from "../pages/covercrypt/DecryptData"
+import EncryptData from "../pages/covercrypt/EncryptData"
+import GenerateUserDecryptionKey from "../pages/covercrypt/GenerateUserDecryptionKey"
+import RefreshKeys from "../pages/covercrypt/RefreshKeys"
+import SetupCovercrypt from "../pages/covercrypt/SetupCovercrypt"
+import AboutCse from "../pages/cse/AboutCse"
+import SetupCse from "../pages/cse/SetupCse"
+import SummarizeDocument from "../pages/cse/SummarizeDocument"
+import AboutFindex from "../pages/findex/AboutFindex"
+import IndexDatabase from "../pages/findex/IndexDatabase"
+import InstantiateFindex from "../pages/findex/InstantiateFindex"
+import SearchInDatabase from "../pages/findex/SearchInDatabase"
+import SetupFindex from "../pages/findex/SetupFindex"
+import AboutPKI from "../pages/pki/AboutPKI"
+import DecryptDataPKI from "../pages/pki/DecryptDataPKI"
+import EncryptDataPki from "../pages/pki/EncryptDataPki"
+import GetCertificate from "../pages/pki/GetCertificate"
+import GrantAccess from "../pages/pki/GrantAccess"
+import ImportAndUnwrapUDK from "../pages/pki/ImportAndUnwrapUDK"
+import SendWrappedDecryptionKey from "../pages/pki/SendWrappedDecryptionKey"
+import SetupPki from "../pages/pki/SetupPki"
+import UploadCert from "../pages/pki/UploadCert"
 
 export interface RoutePath {
   path: string;
@@ -42,11 +42,12 @@ export interface RoutePaths {
 
 export const topSectionTitles = [
   "Overview",
-  "Encrypt with Access Policies",
-  "Search Encrypted Data",
-  "Distribute keys",
-  "Compute using Encrypted Code and Data",
   "Encrypt Client-Side",
+  "Search Encrypted Data",
+  "Compute using Encrypted Code and Data",
+  "Encrypt with Access Policies",
+  "Distribute keys",
+
 ];
 
 export const routePathsConfig: RoutePaths = {
@@ -64,6 +65,82 @@ export const routePathsConfig: RoutePaths = {
       path: "overview",
       label: "Overview",
       component: <OverView />,
+    },
+  ],
+  "client-side-encryption": [
+    {
+      path: "about-cse",
+      label: "About Client-side Encryption",
+      component: <AboutCse />,
+    },
+    {
+      path: "set-up-service",
+      label: "Set up the service",
+      component: <SetupCse />,
+    },
+    {
+      path: "get-summary",
+      label: "Get summary",
+      component: <SummarizeDocument />,
+    },
+  ],
+  "build-encrypted-indexes": [
+    {
+      path: "about-findex",
+      label: "About Cosmian Findex",
+      component: <AboutFindex />,
+    },
+    {
+      path: "set-up-service",
+      label: "Set up service",
+      component: <SetupFindex />,
+    },
+    {
+      path: "instantiate-findex",
+      label: "Instantiate Findex",
+      component: <InstantiateFindex />,
+    },
+    {
+      path: "index-database",
+      label: "Index the database",
+      component: <IndexDatabase />,
+    },
+    {
+      path: "search-in-database",
+      label: "Search words",
+      component: <SearchInDatabase />,
+    },
+  ],
+  "confidential-vm": [
+    {
+      path: "about-cosmian-vm",
+      label: "About Cosmian VM",
+      component: <UseConfidentialVm />,
+    },
+    {
+      path: "deploy-application",
+      label: "Deploy the application and configure Cosmian VM",
+      component: <DeployApplication />,
+    },
+    {
+      path: "snapshot-cosmian-vm",
+      label: "Snapshot the Cosmian VM",
+      component: <SnapshotVm />,
+    },
+    {
+      path: "audit-snapshot",
+      label: "Audit the snapshot",
+      component: <AuditSnapshot />,
+    },
+    {
+      path: "verify-integrity",
+      label: "Verify Cosmian VM integrity",
+      component: <VerifyIntegrity />,
+    },
+    {
+      path: "detect-malicious-activities",
+      label: "Detect malicious activities",
+      component: <DetectMalware />,
     },
   ],
   "encrypt-with-access-policies": [
@@ -106,33 +183,6 @@ export const routePathsConfig: RoutePaths = {
       path: "refresh-keys-for-a-policy",
       label: "Refresh keys for a given policy",
       component: <RefreshKeys />,
-    },
-  ],
-  "build-encrypted-indexes": [
-    {
-      path: "about-findex",
-      label: "About Cosmian Findex",
-      component: <AboutFindex />,
-    },
-    {
-      path: "set-up-service",
-      label: "Set up service",
-      component: <SetupFindex />,
-    },
-    {
-      path: "instantiate-findex",
-      label: "Instantiate Findex",
-      component: <InstantiateFindex />,
-    },
-    {
-      path: "index-database",
-      label: "Index the database",
-      component: <IndexDatabase />,
-    },
-    {
-      path: "search-in-database",
-      label: "Search words",
-      component: <SearchInDatabase />,
     },
   ],
   "distibute-keys": [
@@ -180,55 +230,6 @@ export const routePathsConfig: RoutePaths = {
       path: "decrypt-data",
       label: "Decrypt data",
       component: <DecryptDataPKI />,
-    },
-  ],
-  "confidential-vm": [
-    {
-      path: "about-cosmian-vm",
-      label: "About Cosmian VM",
-      component: <UseConfidentialVm />,
-    },
-    {
-      path: "deploy-application",
-      label: "Deploy the application and configure Cosmian VM",
-      component: <DeployApplication />,
-    },
-    {
-      path: "snapshot-cosmian-vm",
-      label: "Snapshot the Cosmian VM",
-      component: <SnapshotVm />,
-    },
-    {
-      path: "audit-snapshot",
-      label: "Audit the snapshot",
-      component: <AuditSnapshot />,
-    },
-    {
-      path: "verify-integrity",
-      label: "Verify Cosmian VM integrity",
-      component: <VerifyIntegrity />,
-    },
-    {
-      path: "detect-malicious-activities",
-      label: "Detect malicious activities",
-      component: <DetectMalware />,
-    },
-  ],
-  "client-side-encryption": [
-    {
-      path: "about-cse",
-      label: "About Client-side Encryption",
-      component: <AboutCse />,
-    },
-    {
-      path: "set-up-service",
-      label: "Set up the service",
-      component: <SetupCse />,
-    },
-    {
-      path: "get-summary",
-      label: "Get summary",
-      component: <SummarizeDocument />,
     },
   ],
 };
